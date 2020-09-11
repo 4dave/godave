335672cf9eaedae2f618b62b86a22cd478f0e043
Dave Fredkove <spot4dave@gmail.com>
2020-09-11 17:31:47 -0500

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello World 👋🌍</h1>")
+	fmt.Fprintf(w, "<h1>Hello World 👋🌍 - gRun enabled! 👍</h1>")
 }
 
 func about(w http.ResponseWriter, r *http.Request) {
